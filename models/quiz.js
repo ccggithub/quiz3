fd55a9ee6e7386a233c67bc9f5a2e43a5deac07c
ccggithub <ccggithub@gmail.com>
2015-08-03 18:29:21 +0200

    quiz
    quizmod8

---
+++ models/quiz.js
@@ -0,0 +1,20 @@
+// Definicion del modelo de Quiz
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('Quiz',
+		{ 
+			pregunta: {
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg: "--> Falta pregunta"}}
+			 },
+			 respuesta: {
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg: "--> Falta respuesta"}}
+			},
+			tematica: {
+				type: DataTypes.STRING,
+				validate: { notEmpty: {msg: "-->Debe indicar la tematica"}}
+			}
+		}
+	);
+}
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controllers/quiz_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz', errors: [] });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizID
+
+router.get('/author',			quizController.author);
+
+router.get('/quizes',				quizController.index);
+router.get('/quizes/:quizId(\\d+)',		quizController.show);
+router.get('/quizes/:quizId(\\d+)/edit',        quizController.edit)
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
+
+router.get('/quizes/new', 			quizController.new);
+router.post('/quizes/create', 			quizController.create);
+router.put('/quizes/:quizId(\\d+)',		quizController.update);
+router.delete('/quizes/:quizId(\\d+)',		quizController.destroy);
+
+module.exports = router;
